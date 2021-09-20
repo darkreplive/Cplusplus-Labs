4fb25c5dc5de0f84050c86539ecc342c6bca0561
darkreplive <darkreplive@gmail.com>
2021-09-19 22:23:46 -0400

    Finished constructors

--- Week2.cpp
+++ Week2.cpp
@@ -5,6 +5,22 @@
 //test
 using namespace std;
 
+namespace cst8219{
+class Vehicle {
+private:
+	int numWheels;
+	int numDoors;
+public:
+	Vehicle(int w, int d) {
+		numDoors = d;
+		numWheels = w;
+	}
+	Vehicle(int w) : Vehicle(4,4){}
+	Vehicle() : Vehicle(4){}
+};
+}
+
+
 int main(int argc, char** argv)
 {
 	cout << "Hello World!" << endl;

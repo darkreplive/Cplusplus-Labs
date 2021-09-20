c4774f4cdd82cb71e23f57c82353fe8f70b87070
darkreplive <darkreplive@gmail.com>
2021-09-20 00:03:24 -0400

    #Demo 1

--- Week2.cpp
+++ Week2.cpp
@@ -1,28 +1,34 @@
-﻿// Lab2.cpp : Defines the entry point for the application.
+﻿// Lab2.cpp : Using Git
 //
 
 #include<iostream>
-//test
 using namespace std;
 
-namespace cst8219{
-class Vehicle {
-private:
-	int numWheels;
-	int numDoors;
-public:
-	Vehicle(int w, int d) {
-		numDoors = d;
-		numWheels = w;
-	}
-	Vehicle(int w) : Vehicle(4,4){}
-	Vehicle() : Vehicle(4){}
-};
+namespace cst8219 {
+	class Vehicle {
+	private:
+		int numWheels;
+		int numDoors;
+	public:
+		Vehicle(int w, int d) {
+			numDoors = d;
+			numWheels = w;
+			cout << "In constructor with 2 parameters" << endl;
+		}
+		Vehicle(int w) : Vehicle(4, 4) { cout << "In constructor with 1 parameters, wheels =" << w << endl; }
+		Vehicle() : Vehicle(4) {
+			cout << "In constructor with 0 parameters" << endl;
+		}
+		~Vehicle() {
+			cout << "In destructor" << endl;
+		}
+	};
 }
 
 
 int main(int argc, char** argv)
 {
+	cst8219::Vehicle myVehicle();
 	cout << "Hello World!" << endl;
 	return 0;
 }

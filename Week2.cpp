64fa844e74ffef5fdf3226187115bd7ea0cca3ba
unknown <darkreplive@gmail.com>
2021-09-19 20:22:47 -0400

    Initial commit

---
+++ Week2.cpp
@@ -0,0 +1,10 @@
+﻿// Lab2.cpp : Defines the entry point for the application.
+//
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
